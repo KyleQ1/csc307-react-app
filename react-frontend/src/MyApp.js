47a21690d3104c5b7ad2080bd87a16c4bd0a26c6
KyleQ1 <kylequinlan09@gmail.com>
2023-09-27 15:14:21 -0700

    Add input fields

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,24 +1,9 @@
 import React, {useState} from "react";
-import Table from "./Table"
+import Table from "./Table";
+import Form from './Form';
   
   function MyApp() {
     const [characters, setCharacers] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor",
-        },
-        {
-          name: "Mac",
-          job: "Bouncer",
-        },
-        {
-          name: "Dee",
-          job: "Aspring actress",
-        },
-        {
-          name: "Dennis",
-          job: "Bartender",
-        },
       ]);
     function removeOneCharacter(index) {
         const updated = characters.filter((characters, i) => {
@@ -30,6 +15,7 @@ import Table from "./Table"
       <div className="container">
         <Table characterData={characters} 
                 removeCharacter={removeOneCharacter} />
+        <Form />
       </div>
     );
   }
