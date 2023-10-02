e44ae24d457fea1567b2278d53a652c87a720f8e
KyleQ1 <kylequinlan09@gmail.com>
2023-10-02 16:57:05 -0700

    Find user by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,7 +40,7 @@ const users = {
     ]
  }
  
- const findUserByName = (name) => { 
+const findUserByName = (name) => { 
     return users['users_list']
         .filter( (user) => user['name'] === name); 
 }
@@ -54,6 +54,20 @@ app.get('/users', (req, res) => {
     }
     else{
         res.send(users);
+    }
+});
+
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+    
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
     }
 });
 
